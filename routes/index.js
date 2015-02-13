5b604024c71b5720e8812a1ea6d22021f1706e91
Dong Yuwei <yuwei.dong@rea-group.com>
2015-02-13 10:12:39 +0800

    update logout route

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ router.get('/login', function(req, res) {
 router.get('/logout', passwordless.logout(),
     function(req, res) {
         res.clearCookie('uid');
-        res.redirect(config.application + 'join');
+        res.redirect(config.application + 'login');
     });
 
 /* POST login screen. */
