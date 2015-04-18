115fe069f78d68acbf1b89129098458c38003ecf
Dong Yuwei <yuwei.dong@rea-group.com>
2015-04-18 21:07:35 +0800

    open web ui of maildev

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,9 @@
 npm install
 
 kill -9 $(lsof -i:1025 -t) 2> /dev/null
-node_modules/maildev/bin/maildev & # visit http://127.0.0.1:1080
+node_modules/maildev/bin/maildev & 
+
+# visit http://127.0.0.1:1080
+open http://127.0.0.1:1080 2> /dev/null
 
 NODE_ENV=development SMTP_SERVER=127.0.0.1 SMTP_PORT=1025  node app.js
